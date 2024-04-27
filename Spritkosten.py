e497fde18564bce5b93372effedbbed7d3819c60
Hannes Bröhl <hannes.broehl@icloud.com>
2024-04-27 15:54:52 +0200

    einfaches Programm zum berechnen der Spritkosten mittels Eingabe.

---
+++ Spritkosten.py
@@ -0,0 +1,12 @@
+start_price = 4.8
+km = int(input("Enter a number: "))
+
+if km > 5:
+    costs = 1.7
+else:
+    costs = 2.1
+
+costs = 2.1
+total_expenses = start_price + (costs * km)
+
+print("Deine Kosten sind: ", total_expenses )
