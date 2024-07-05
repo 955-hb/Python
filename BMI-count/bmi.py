548f21b3037dae78938ec20ef30d54fa7d7e564d
Hannes Bröhl <hannes.broehl@icloud.com>
2024-07-05 11:24:28 +0200

    small BMI-calculator

---
+++ BMI-count/bmi.py
@@ -0,0 +1,33 @@
+# Schreibe ein rogramm welches die Größe und Gewicht einer Person (mittels input) abfragt
+# und auf dieser Basis den BMI berechnet und ausgibt.
+# Formel = (Gewicht in kg) / (Körpergröße im m)^2
+
+
+print('Berechne deinen BMI-Wert')
+
+age = input('Wie alt bist du?')
+height_str = input('Wie groß bist du? (in m)')
+weight_str = input('Wie schwer bist du? (in kg)')
+
+height = float(height_str)
+weight = float(weight_str)
+
+result_bmi = weight / height **2
+
+bmi = round(result_bmi, 2)
+print('Mit Ihren alter von ' + str(age) + ' haben Sie einen BMI-Wert von: ' + str(bmi) + '!')
+
+if bmi < 16:
+    print('Sie haben ein kritisches Untergewicht!')
+elif bmi >= 16 and bmi <= 18.5:
+    print('Sie haben leichtes Untergewicht!')
+elif bmi >= 18.5 and bmi <= 25:
+    print('Sie haben ein Normalgewicht!')
+elif bmi >= 25 and bmi <= 30:
+    print('Sie haben leichtes Übergewicht!')
+elif bmi > 30:
+    print('Sie haben erhöhtes Übergewicht!')
+
+input('drücken Sie eine beliebige Taste zum Beenden des Programmes!')
+
+
